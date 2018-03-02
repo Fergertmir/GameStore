b53943556ddb5bdbdb68cfe80057e7df56453ef9
dreko95 <fergertmir@gmail.com>
2018-03-03 00:07:18 +0200

    Added admin panel + CRUD.

--- GameStore.Domain/Entities/Game.cs
+++ GameStore.Domain/Entities/Game.cs
@@ -1,22 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
 
 namespace GameStore.Domain.Entities
 {
     public class Game
     {
-        [Key]
+        [HiddenInput(DisplayValue = false)]
         public int GameId { get; set; }
-        [Required]
+
         [StringLength(100)]
+        [Display(Name = "Название")]
+        [Required(ErrorMessage = "Пожалуйста, введите название игры")]
         public string Name { get; set; }
-        [Required]
+
+        [DataType(DataType.MultilineText)]
         [StringLength(500)]
+        [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Пожалуйста, введите описание для игры")]
         public string Description { get; set; }
-        [Required]
+
         [StringLength(50)]
+        [Display(Name = "Категория")]
+        [Required(ErrorMessage = "Пожалуйста, укажите категорию для игры")]
         public string Category { get; set; }
+
         [Required]
+        [Display(Name = "Цена (руб)")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
     }
 }
---
+++ GameStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IGameRepository repository;
+
+        public AdminController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Edit(int gameId)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+            return View(game);
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Games);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveGame(game);
+                if (game.GameId == 0)
+                {
+                    TempData["message"] = string.Format("Добавлен товар \"{0}\".", game.Name);
+                }
+                else
+                {
+                    TempData["message"] = string.Format("Изменения в товаре \"{0}\" были сохранены.", game.Name);
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Что-то не так со значениями данных
+                return View(game);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Game());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int gameId)
+        {
+            Game deletedGame = repository.DeleteGame(gameId);
+            if (deletedGame != null)
+            {
+                TempData["message"] = string.Format("Товар \"{0}\" был удалена.",
+                    deletedGame.Name);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
