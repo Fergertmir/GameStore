cc031b5f4f84f92a04a6619df3e06bd50e7f6b55
dreko95 <fergertmir@gmail.com>
2018-03-07 00:59:15 +0200

    Added admin authentication.

--- GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,6 +5,8 @@ using Ninject;
 using GameStore.Domain.Concrete;
 using GameStore.Domain.Abstract;
 using System.Configuration;
+using GameStore.WebUI.Infrastructure.Abstract;
+using GameStore.WebUI.Infrastructure.Concrete;
 
 namespace GameStore.WebUI.Infrastructure
 {
@@ -43,6 +45,7 @@ namespace GameStore.WebUI.Infrastructure
             //   в параметр "settings" будет внедрен объект emailSettings.
             kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
                 .WithConstructorArgument("settings", emailSettings);
+            kernel.Bind<IAuthProvider>().To<FormAuthProvider>();
         }
     }
 }
